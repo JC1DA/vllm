d9334ad13f8c43fa4f21f1d84bc4f86bce58aa1c
JC1DA <jc1da.3011@gmail.com>
2024-10-29 00:57:15 -0700

    Add guidance backend as json/regex guided generation

---
+++ vllm/model_executor/guided_decoding/guidance_decoding.py
@@ -0,0 +1,74 @@
+from enum import Enum
+from vllm.model_executor.guided_decoding.guidance_logits_processors import (
+    GuidanceLogitsProcessor
+)
+from vllm.sampling_params import GuidedDecodingParams
+from transformers import PreTrainedTokenizerBase
+from typing import Tuple, Union
+from json import dumps as json_dumps
+from re import escape as regex_escape
+
+class GuidedDecodingMode(Enum):
+    JSON = "json"
+    REGEX = "regex"
+    CHOICE = "choice"
+    GRAMMAR = "grammar"
+
+
+def get_local_guidance_guided_decoding_logits_processor(
+    guided_params: GuidedDecodingParams, tokenizer: PreTrainedTokenizerBase
+) -> Union[GuidanceLogitsProcessor, None]:
+    """
+    Given an OpenAI-compatible request, check for guided decoding parameters
+    and get the necessary logits processor for the given guide.
+    We cache logit processors by (guide, tokenizer), and on cache hit
+    we make a shallow copy to reuse the same underlying FSM.
+    """
+    guide, mode = _get_guide_and_mode(guided_params)
+    if not guide or not mode:
+        return None
+
+    return _get_logits_processor(
+        guide, tokenizer, mode, guided_params.whitespace_pattern
+    )
+
+
+def _get_guide_and_mode(
+    guided_params: GuidedDecodingParams,
+) -> Union[Tuple[str, GuidedDecodingMode], Tuple[None, None]]:
+    if guided_params.json:
+        if isinstance(guided_params.json, dict):
+            # turn dict into hashable string
+            json = json_dumps(guided_params.json)
+        else:
+            json = guided_params.json
+        return json, GuidedDecodingMode.JSON
+    elif guided_params.regex:
+        return guided_params.regex, GuidedDecodingMode.REGEX
+    elif guided_params.choice:
+        # choice just uses regex
+        choices = [regex_escape(str(choice)) for choice in guided_params.choice]
+        choices_regex = "(" + "|".join(choices) + ")"
+        return choices_regex, GuidedDecodingMode.CHOICE
+    # elif guided_params.grammar:
+    #     return guided_params.grammar, GuidedDecodingMode.GRAMMAR
+    # elif guided_params.json_object:
+    #     return JSON_GRAMMAR, GuidedDecodingMode.GRAMMAR
+    else:
+        return None, None
+
+
+def _get_logits_processor(
+    guide: str,
+    tokenizer: PreTrainedTokenizerBase,
+    mode: GuidedDecodingMode,
+    whitespace_pattern: Union[str, None],
+) -> Union[GuidanceLogitsProcessor, None]:
+    if mode == GuidedDecodingMode.JSON:
+        return GuidanceLogitsProcessor(mode.value, guide, tokenizer, whitespace_pattern)
+    elif mode == GuidedDecodingMode.REGEX or mode == GuidedDecodingMode.CHOICE:
+        return GuidanceLogitsProcessor(mode.value, guide, tokenizer)
+    # elif mode == GuidedDecodingMode.GRAMMAR:
+    #     return CFGLogitsProcessor(guide, tokenizer)
+    else:
+        raise ValueError(f"Unknown guided decoding mode {mode}")
---
+++ vllm/model_executor/guided_decoding/guidance_logits_processors.py
@@ -0,0 +1,152 @@
+import os
+import time
+import guidance
+import torch
+import json
+import numpy as np
+import llguidance  # type: ignore[import-untyped]
+from pydantic import BaseModel
+from typing import Type, Union, List
+from guidance.models import TransformersTokenizer
+from guidance._schema import LLInterpreterResponse
+from transformers import PreTrainedTokenizerBase
+
+class GuidanceLogitsProcessor:
+    metadata = {}
+
+    def __init__(
+        self,
+        mode: str,
+        guide: Union[dict, Type[BaseModel], str],
+        tokenizer: PreTrainedTokenizerBase,
+        whitespace_pattern: Union[str, None] = None,
+    ) -> None:
+        self.mode = mode
+        self.initialized = False
+        self.guide = guide
+        self.tokenizer = tokenizer
+        self.tokenizer_name = tokenizer.name_or_path
+        self.whitespace_pattern = whitespace_pattern
+
+        self.is_stopped = False
+        self.pending_ff_tokens: list[int] = []
+        self.new_sampling = False
+
+    def initialize(self):
+        if self.initialized:
+            return
+        
+        if self.mode.lower() == "json":
+            if isinstance(self.guide, str):
+                schema = json.loads(self.guide)
+            else:
+                schema = self.guide
+        
+            self.schema = guidance.json(schema=schema, temperature=0.0)
+            self.serialized_grammar = self.schema.ll_serialize()
+        elif self.mode.lower() in ["regex", "choice"]:
+            self.serialized_grammar = guidance.gen(regex=self.guide, temperature=0.0).ll_serialize()
+
+        t0 = time.time()
+        if "guidance_tokenizer" not in self.metadata:
+            self.metadata["guidance_tokenizer"] = TransformersTokenizer(
+                model=self.tokenizer.name_or_path, transformers_tokenizer=self.tokenizer
+            )        
+        self.guidance_tokenizer = self.metadata["guidance_tokenizer"]
+        print("GuidanceTokenizer init time:", time.time() - t0)
+
+        t0 = time.time()
+        if "ll_tokenizer" not in self.metadata:            
+            self.metadata["ll_tokenizer"] = llguidance.LLTokenizer(
+                llguidance.TokenizerWrapper(self.guidance_tokenizer)
+            )
+        self.ll_tokenizer = self.metadata["ll_tokenizer"]
+        print("LLTokenizer init time:", time.time() - t0)
+        
+        t0 = time.time()
+        self.ll_interpreter = llguidance.LLInterpreter(
+            self.ll_tokenizer,
+            json.dumps(self.serialized_grammar),
+            enable_backtrack=False,
+            log_level=int(os.environ.get("LLGUIDANCE_LOG_LEVEL", "1")),
+        )
+        print("LLInterpreter init time:", time.time() - t0)
+        
+        self.initialized = True
+
+    def __call__(
+        self,
+        prompt_tokens_ids: List[int],
+        past_tokens_ids: list[int],
+        logits: torch.Tensor,
+    ) -> torch.Tensor:
+        self.initialize()
+
+        if self.is_stopped:
+            return logits
+
+        try:
+            if len(past_tokens_ids) == 0:
+                self.ll_interpreter.process_prompt(prompt_tokens_ids)
+
+            if self.new_sampling:
+                backtrack, ff_tokens = self.ll_interpreter.post_process(
+                    past_tokens_ids[-1]
+                )
+                if len(ff_tokens) > 0 and backtrack == 0:
+                    ff_tokens = ff_tokens[1:]
+                self.pending_ff_tokens.extend(ff_tokens)
+                self.new_sampling = False
+
+                # backtrack is disabled by default
+                # assert backtrack == 0
+
+            if len(self.pending_ff_tokens) > 0:
+                ff_token = self.pending_ff_tokens.pop(0)
+                masked_logits = torch.zeros_like(
+                    logits, dtype=logits.dtype, device=logits.device
+                )
+                masked_logits[ff_token] = 200.0
+                return masked_logits
+
+            mask, resp = self.ll_interpreter.mid_process()
+            # if mask is None:
+            #     self.ll_interpreter.post_process(None)
+
+            r = LLInterpreterResponse.model_validate_json(resp)
+            # response = r.progress.to_engine_call_response()
+
+            if r.stop:
+                mask = torch.zeros_like(
+                    logits, dtype=logits.dtype, device=logits.device
+                )
+                mask[self.guidance_tokenizer.eos_token_id] = 200.0
+                self.is_stopped = True
+            elif mask is None:
+                mask = (
+                    torch.zeros_like(logits, dtype=logits.dtype, device=logits.device)
+                )
+            else:
+                mask = np.frombuffer(mask, dtype=np.uint8)
+                mask = torch.tensor(mask, dtype=logits.dtype, device=logits.device)
+
+            if mask.shape[0] != logits.shape[0]:
+                # Some model has extra tokens that are not in the vocabulary
+                mask = torch.cat(
+                    [
+                        mask,
+                        torch.ones(
+                            logits.shape[0] - mask.shape[0],
+                            device=logits.device,
+                        )
+                        * 200.0,
+                    ]
+                )
+
+            masked_logits = logits + mask
+            self.new_sampling = True
+        except Exception as e:
+            print("Error in GuidanceJsonLogitsProcessor: ", e)
+            raise e
+
+        return masked_logits
