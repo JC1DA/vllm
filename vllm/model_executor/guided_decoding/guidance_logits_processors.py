fix masked_logits calculation
Signed-off-by: Loc Huynh <jc1da.3011@gmail.com>

vllm/model_executor/guided_decoding/guidance_logits_processors.py
@@ -181,7 +181,7 @@ class GuidanceLogitsProcessor:
                     ),
                 ])
 
-            masked_logits = logits + mask
+            masked_logits = (logits - torch.min(logits)) * mask
             self.new_sampling = True
         except Exception as e:
             raise e